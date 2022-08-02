e01605b92a8331cd9bc6f71af934cf431e5cd1d2
bolianchen <bolianchen@gmail.com>
2022-08-02 23:31:04 +0800

    Finish assignment 1
    Complete Soundex search

--- cs106b/assignments/2022su/assignment1/main.cpp
+++ cs106b/assignments/2022su/assignment1/main.cpp
@@ -10,10 +10,10 @@ int main() {
     if (runSimpleTests(SELECTED_TESTS)) {
         return 0;
     }
-    findPerfects(40000);
+    // findPerfects(40000);
     // Comment out the above line and uncomment below line
     // to switch between running perfect.cpp and soundex.cpp
-    // soundexSearch("res/surnames.txt");
+    soundexSearch("res/surnames.txt");
 
     cout << endl << "main() completed." << endl;
     return 0;
